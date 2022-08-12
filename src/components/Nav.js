e4a89f45ae726f76b1e6999f0ad555305a526f72
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-12 15:52:28 +0100

    create Nav component function

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Nav() {
+  return ( 
+    <header>
+      <nav>
+        <ul>
+          <li className="nav-brand">Bookstore</li>
+        </ul>
+        <ul>
+          <li>Books</li>
+          <li>Catigory</li>
+        </ul>
+      </nav>
+    </header>
+   );
+}
+
+export default Nav;
