e590b26955ba248772d14f7eb033f562d5908ac7
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-12 18:23:40 +0100

    add map method to book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 function Book(props) {
-  const [title, author] = props;
+  const {title, author} = props;
   return (
     <div className="book">
       <p>
         <span>{title}</span>
         {' '}
         by
+        {' '}
         <span>{author}</span>
       </p>
     </div>
--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Book from './Book';
 
 function BookContainer() {
-  const books = {
+  const myBooks = [{
     title: 'Habits in creation and destruction',
     author: 'John Smith',
-  };
+    id: 1,
+  }];
   return (
     <main>
       <div className="book-container">
         <ul>
           <li className="book-item">
-
-            <Book author={books.author} title={books.title} />
+            {myBooks.map((book) => {
+              return <Book key={book.id} author={book.author} title={book.title} />
+            })}
             <button className="remove" type="button">Remove</button>
           </li>
         </ul>
