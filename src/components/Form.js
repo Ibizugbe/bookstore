3290256ec5987d78f4f739d3144f602ac8a64705
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-17 13:34:28 +0100

    use 'useDispatch and useSate in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,46 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
-import { addBOOK } from '../redux/books/books';
+import { addBook } from '../redux/books/books';
 
 function Form() {
+  const dispatch = useDispatch();
+
+  const [formState, setFormState] = useState({ title: '', author: '' });
+
+  const changeState = (e) => {
+    e.preventDefault();
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const bookState = (e) => {
+    e.preventDefault();
+    if (!formState.title.trim() || !formState.author.trim()) return;
+    const book = {
+      id: uuidv4(),
+      title: formState.title,
+      author: formState.author,
+    };
+    dispatch(addBook(book));
+    setFormState({ title: '', author: '' });
+  };
+
   return (
-    <form action="#">
-      <input type="text" placeholder="Author" />
-      <input type="text" placeholder="Title" />
+    <form action="#" onSubmit={bookState}>
+      <input
+        type="text"
+        onChange={changeState}
+        value={formState.author}
+        placeholder="Author"
+        name="Author"
+      />
+      <input
+        type="search"
+        onChange={changeState}
+        value={formState.title}
+        placeholder="title"
+        name="title"
+      />
       <input type="submit" value="Add Book" />
     </form>
   );
