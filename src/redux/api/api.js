def24e7925e7942840728d0ee8641bcd7f8a63c9
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-19 13:58:30 +0100

    fix linter errors

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,2 +1,2 @@
-const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/CWxPz8ZnKTUCmLORwBhL/books';
+const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/CWxPz8ZnKTUCmLORwBhL';
 export default apiUrl;
