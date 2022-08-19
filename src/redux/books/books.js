7e8c14465ad8f4c4f5289be8c25f1da6b9d4214a
ibizugbe <abrahamibizugbe@gmail.com>
2022-08-19 12:38:00 +0100

    create function to add and remove book from api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,28 +4,46 @@ import apiUrl from './src/api/api';
 
 const ADD_BOOK = 'bookstore/books/addBook';
 const REMOVE_BOOK = 'bookstore/books/removeBook';
-const FETCHBOOKS = 'bookstore/books/fetchBooks';
+const FETCH_BOOKS = 'bookstore/books/fetchBooks';
 
 const initialState = { books: [] };
 export default function bookReducer(state = initialState, action) {
   switch (action.type) {
-    // add action
-    case ADD_BOOK: return [
-      ...state, action.book,
-    ];
-    // remove action
-    case REMOVE_BOOK: return state.filter((book) => book.id !== action.id);
-    // when the action is not defined
-    default: return state;
+    case `${FETCH_BOOKS}/fulfilled`:
+      return { ...state, books: action.payload };
+    case `${ADD_BOOK}/fulfilled`:
+      return { ...state, books: [...state.books, action.payload] };
+    case `${REMOVE_BOOK}/fulfilled`:
+      return { ...state, books: state.books.filter((book) => book.item_id !== action.payload) };
+    default:
+      return state;
   }
 }
 
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  book,
+const transformData = (myObj) => Object.keys(myObj).map((obj) => ({
+  item_id: obj,
+  title: myObj[obj][0].title,
+  author: myObj[obj][0].author,
+  category: myObj[obj][0].category,
+}));
+
+export const fetchBooks = createAsyncThunk(FETCH_BOOKS, async () => {
+  const res = await fetch(`${apiUrl}/books`);
+  const data = await res.json();
+
+  const toSend = transformData(data);
+
+  return toSend;
 });
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  id,
+export const addBook = createAsyncThunk(ADD_BOOK, async (bookInfo) => {
+  await axios.post(`${apiUrl}/books`, bookInfo);
+
+  return bookInfo;
+});
+
+export const removeBook = createAsyncThunk(REMOVE_BOOK, async (id) => {
+  await axios.delete(`${apiUrl}/books/${id}`, { item_id: id });
+
+  return id;
 });
